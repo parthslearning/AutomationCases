19d435364a895c62c93744848f12165fedf7ed86
admin <admin@DESKTOP-59NR59S>
2023-05-05 23:44:06 +0530

    Commit-1

---
+++ Object Repository/OverflowMenu/Blog.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Blog</name>
+   <tag></tag>
+   <elementGuidId>095c2c2a-c8e9-46d2-be28-2bdb6339a3a9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[normalize-space()='Blog']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
